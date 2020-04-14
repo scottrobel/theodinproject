8f696cc821c3a94384ba05feb92b0a37e8f75d62
scottrobel <robelrain@gmail.com>
2020-04-14 21:36:21 +0000

    create known_completion_durations method

--- app/services/lesson_completion_data.rb
+++ app/services/lesson_completion_data.rb
@@ -17,4 +17,24 @@ class LessonCompletionData
       section.lessons.sort_by(&:position)
     end.flatten
   end
+
+  def known_completion_durations
+    lessons = ordered_lessons
+    current_lesson = lessons.shift
+    current_lesson_avg_completion_time = lesson_and_avg_completion_date_pairs[current_lesson.id]
+    completion_durations = {}
+    lessons.each do |lesson|
+      next_lesson = lesson
+      next_lesson_avg_completion_time = lesson_and_avg_completion_date_pairs[next_lesson.id]
+      if next_lesson_avg_completion_time.nil? # if noone completed the assignment return data
+        break
+      else
+        average_duration_to_finish_current_lesson = next_lesson_avg_completion_time - current_lesson_avg_completion_time
+        completion_durations[current_lesson] = ActiveSupport::Duration.build(average_duration_to_finish_current_lesson)
+        current_lesson_avg_completion_time = next_lesson_avg_completion_time
+        current_lesson = next_lesson
+      end
+    end
+    completion_durations
+  end
 end
