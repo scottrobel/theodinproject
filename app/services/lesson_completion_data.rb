4a90545a8927d98c884c6a501ab6d9c8879b14da
scottrobel <robelrain@gmail.com>
2020-04-14 21:29:22 +0000

    create ordered_lessons method

--- app/services/lesson_completion_data.rb
+++ app/services/lesson_completion_data.rb
@@ -11,4 +11,10 @@ class LessonCompletionData
   def lesson_and_avg_completion_date_pairs
     @lesson_and_avg_completion_date_pairs ||= LessonCompletion.all.group(:lesson_id).average('extract(epoch from created_at)')
   end
+
+  def ordered_lessons
+    @ordered_lessons ||= @course.sections.includes(:lessons).map do |section|
+      section.lessons.sort_by(&:position)
+    end.flatten
+  end
 end
