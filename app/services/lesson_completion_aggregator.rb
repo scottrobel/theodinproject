b66bb376bb064e4aed59cd04a8523a1492622f01
scottrobel <robelrain@gmail.com>
2020-04-19 00:45:45 +0000

    create lesson_completions method

--- app/services/lesson_completion_aggregator.rb
+++ app/services/lesson_completion_aggregator.rb
@@ -3,6 +3,12 @@ class LessonCompletionAggregator
     @lessons = lessons
   end
 
+  private
+
+  def lesson_completions
+    LessonCompletions.where(id: lesson_ids)
+  end
+
   def lesson_ids
     @lesson_ids ||= lessons.pluck(:id)
   end
