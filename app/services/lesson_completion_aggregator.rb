86ea323b4ba151be46366995ceb46becbb8cda34
scottrobel <robelrain@gmail.com>
2020-04-19 01:00:09 +0000

    create filtered_lesson_completions method

--- app/services/lesson_completion_aggregator.rb
+++ app/services/lesson_completion_aggregator.rb
@@ -1,9 +1,23 @@
 class LessonCompletionAggregator
-  def initialize(lessons)
+  def initialize(lessons, lesson_completion_filter = nil)
     @lessons = lessons
   end
 
+  def lesson_completions_count
+    filtered_lesson_completions\
+    .group(:lesson_id)\
+    .count
+  end
+
   private
+
+  def filtered_lesson_completions
+    if lesson_completion_filter
+      lesson_completions.where(**lesson_completion_filter)
+    else
+      lesson_completions
+    end
+  end
 
   def lesson_completions
     LessonCompletions.where(id: lesson_ids)
